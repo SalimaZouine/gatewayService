bd0f76fe527cea4d598fa27dd874743984dff3c0
SalimaZouine <ZOUINE.SALIMA@emsi-edu.ma>
2020-12-29 13:27:44 +0100

    first commit

---
+++ src/main/java/org/sid/gateway/GatewayApplication.java
@@ -0,0 +1,43 @@
+package org.sid.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+   // @Bean
+   /* RouteLocator routeLocator(RouteLocatorBuilder builder){
+        return  builder.routes()
+                .route((r) -> r.path("/customers/**").uri("http://localhost:8081/").id("r1"))
+                .route((r) -> r.path("/products/**").uri("http://localhost:8082/").id("r2"))
+                .build();
+    }*/
+
+    //et biensur je dois faire cette methode si je connais le nom du MICROService
+   // @Bean
+   RouteLocator routeLocator(RouteLocatorBuilder builder){
+        return  builder.routes()
+                .route((r) -> r.path("/customers/**").uri("lb://CUSTOMER-SERVICE").id("r1"))
+                .route((r) -> r.path("/products/**").uri("lb://PRODUCT-SERVICE").id("r2"))
+                .build();
+    }
+
+    //Dynamiquement
+    @Bean
+    DiscoveryClientRouteDefinitionLocator definitionLocator(
+            ReactiveDiscoveryClient rdc,
+            DiscoveryLocatorProperties properties){
+       return  new DiscoveryClientRouteDefinitionLocator(rdc,properties);
+    }
+}
